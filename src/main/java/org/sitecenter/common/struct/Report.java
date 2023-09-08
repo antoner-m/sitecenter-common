07d18ab6da22843fb86115ca4506f1ea071b4a41
antoner-m <me@antoner.me>
2023-09-08 22:36:30 +0300

    Report now thread-safe
    synchronized row methods.

--- src/main/java/org/sitecenter/common/struct/Report.java
+++ src/main/java/org/sitecenter/common/struct/Report.java
@@ -21,45 +21,63 @@ public class Report {
         this.date = LocalDateTime.now();
     }
     public Report addRow(@NonNull String group, @NonNull String code, @NonNull String name, String value, int status, String comment) {
-        getRows().add(new ReportRow(group, code, name, value, status, comment));
+        synchronized (rows) {
+            getRows().add(new ReportRow(group, code, name, value, status, comment));
+        }
         return this;
     }
     public Report addError(@NonNull String group, @NonNull String code, @NonNull String name, String value, String comment) {
-        getRows().add(new ReportRow(group, code, name, value, ReportRow.STATUS_ERROR, comment));
+        synchronized (rows) {
+            getRows().add(new ReportRow(group, code, name, value, ReportRow.STATUS_ERROR, comment));
+        }
         return this;
     }
     public Report addOk(@NonNull String group, @NonNull String code, @NonNull String name, String value, String comment) {
-        getRows().add(new ReportRow(group, code, name, value, ReportRow.STATUS_OK, comment));
+        synchronized (rows) {
+            getRows().add(new ReportRow(group, code, name, value, ReportRow.STATUS_OK, comment));
+        }
         return this;
     }
     public Report addInfo(@NonNull String group, @NonNull String code, @NonNull String name, String value, String comment) {
-        getRows().add(new ReportRow(group, code, name, value, ReportRow.STATUS_INFO, comment));
+        synchronized (rows) {
+            getRows().add(new ReportRow(group, code, name, value, ReportRow.STATUS_INFO, comment));
+        }
         return this;
     }
     public Report addWarn(@NonNull String group, @NonNull String code, @NonNull String name, String value, String comment) {
-        getRows().add(new ReportRow(group, code, name, value, ReportRow.STATUS_WARN, comment));
+        synchronized (rows) {
+            getRows().add(new ReportRow(group, code, name, value, ReportRow.STATUS_WARN, comment));
+        }
         return this;
     }
 
     public Report addGroup(@NonNull String code, @NonNull  String name) {
-        getGroups().put(code, name);
+        synchronized (groups) {
+            getGroups().put(code, name);
+        }
         return this;
     }
     public ReportGroup rowsByGroup(@NonNull String groupCode) {
-        List<ReportRow> groupRows = getRows().stream().filter(row -> groupCode.equals(row.getGroup())).collect(Collectors.toList());
-        return new ReportGroup(groupCode, getGroups().get(groupCode), groupRows);
+        synchronized (groups) {
+            List<ReportRow> groupRows = getRows().stream().filter(row -> groupCode.equals(row.getGroup())).collect(Collectors.toList());
+            return new ReportGroup(groupCode, getGroups().get(groupCode), groupRows);
+        }
     }
     public List<ReportGroup> reportGroups() {
-        initGroups();
-        return getGroups().keySet().stream().map(this::rowsByGroup).collect(Collectors.toList());
+        synchronized (groups) {
+            initGroups();
+            return getGroups().keySet().stream().map(this::rowsByGroup).collect(Collectors.toList());
+        }
     }
 
     /** Init group codes from rows in case when reporter forget to init group codes/names */
     private void initGroups() {
-        for(ReportRow row : rows) {
-            String groupCode = row.getGroup();
-            if (!getGroups().containsKey(groupCode))
-                getGroups().put(groupCode,"");
+        synchronized (groups) {
+            for (ReportRow row : rows) {
+                String groupCode = row.getGroup();
+                if (!getGroups().containsKey(groupCode))
+                    getGroups().put(groupCode, "");
+            }
         }
     }
 }
