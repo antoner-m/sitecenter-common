6c5ed9716d48a5b49f6b0dacec17ed9f52985154
antoner-m <me@antoner.me>
2024-01-27 15:57:45 +0300

    fixed some formats

--- src/main/java/org/sitecenter/common/util/LocalDateUtils.java
+++ src/main/java/org/sitecenter/common/util/LocalDateUtils.java
@@ -1,20 +1,23 @@
 package org.sitecenter.common.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 public class LocalDateUtils {
     public static LocalDateTime getDate(String str) {
-        if (str == null || str.length() == 0) return null;
+        if (str == null || str.isEmpty()) return null;
         str = str.trim();
+        if (str.contains("z") || str.contains("t")) str = str.toUpperCase();
         LocalDateTime result = null;
         if (!str.contains("Z") && str.contains("T"))
             try {
@@ -22,14 +25,15 @@ public class LocalDateUtils {
                 return result;
             } catch (Exception ex) {
 //                ex.printStackTrace();
+                log.debug("Error parsing date0:[" + str + "]:", ex);
             }
         if (str.contains("Z") && str.contains("T"))
             try {
-                ZonedDateTime zdt = ZonedDateTime.parse(str);
-                result = zdt.toLocalDateTime();
-                return result;
+                ZonedDateTime dateTime = ZonedDateTime.parse(str);
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
             } catch (Exception ex) {
-                log.error("Error parsing date1:["+str+"]:",ex);
+                log.debug("Error parsing date1:[" + str + "]:", ex);
             }
 
         if (str.contains(".") && str.length() == 10) {
@@ -39,8 +43,7 @@ public class LocalDateUtils {
                 result = new java.sql.Timestamp(df_date.parse(str).getTime()).toLocalDateTime();
                 return result;
             } catch (ParseException ex) {
-                log.error("Error parsing date2:["+str+"]:",ex);
-                ex.printStackTrace();
+                log.debug("Error parsing date2:[" + str + "]:", ex);
             }
         }
         if (str.contains("-") && str.length() == 10) {
@@ -50,7 +53,7 @@ public class LocalDateUtils {
                 result = new java.sql.Timestamp(df_date.parse(str).getTime()).toLocalDateTime();
                 return result;
             } catch (ParseException ex) {
-                log.error("Error parsing date3:["+str+"]:",ex);
+                log.debug("Error parsing date3:[" + str + "]:", ex);
             }
         }
         if (str.contains(" ") && str.length() == 19) {
@@ -60,57 +63,196 @@ public class LocalDateUtils {
                 result = new java.sql.Timestamp(df_date.parse(str).getTime()).toLocalDateTime();
                 return result;
             } catch (ParseException ex) {
-                log.error("Error parsing date4:["+str+"]:",ex);
+                log.debug("Error parsing date4:[" + str + "]:", ex);
             }
         }
         if (str.length() == 25 && str.contains("+") && str.contains("-") && str.contains("T")) {
             try {
-                LocalDateTime dateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssxxx"));
-                return dateTime;
-            } catch (Exception ex){
-                log.error("Error parsing date5:["+str+"]:",ex);
+                ZonedDateTime dateTime = ZonedDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssxxx"));
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
+            } catch (Exception ex) {
+                log.debug("Error parsing date5:[" + str + "]:", ex);
             }
         }
         if (str.contains("+") && str.contains("-") && str.contains("T")) {
             try {
-                LocalDateTime dateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ"));
-                return dateTime;
-            } catch (Exception ex){
-                log.error("Error parsing date6:["+str+"]:",ex);
+                ZonedDateTime dateTime = ZonedDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ"));
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
+            } catch (Exception ex) {
+                log.debug("Error parsing date6:[" + str + "]:", ex);
             }
         }
 
         if (str.contains("+") && str.contains(".") && str.contains("T")) {
             try {
-                LocalDateTime dateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy.MM.dd'T'HH:mm:ssZ"));
-                return dateTime;
+                ZonedDateTime dateTime = ZonedDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy.MM.dd'T'HH:mm:ssZ"));
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
             } catch (Exception ex) {
-                log.error("Error parsing date7:["+str+"]:",ex);
+                log.debug("Error parsing date7:[" + str + "]:", ex);
             }
         }
 
-        if (str.contains("UTC")) {
-                SimpleDateFormat df_date = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH);
-                try {
-                    result = new java.sql.Timestamp(df_date.parse(str).getTime()).toLocalDateTime();
-                    return result;
-                } catch (ParseException ex) {
-                    log.error("Error parsing date8:["+str+"]:",ex);
-                    ex.printStackTrace();
-                }
-//            try {
-//                LocalDateTime dateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss Z yyyy"));
-//                return dateTime;
-//            } catch (Exception ex) {
-//                log.error("Error parsing date7:["+str+"]:",ex);
-//            }
+        if (str.contains("-") && str.contains(":") && str.contains(".") && str.contains(" ")) {
+            try {
+                ZonedDateTime dateTime = ZonedDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss.nZ"));
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
+            } catch (Exception ex) {
+                log.debug("Error parsing date71:[" + str + "]:", ex);
+            }
+            try {
+                ZonedDateTime dateTime = ZonedDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss.nX"));
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
+            } catch (Exception ex) {
+                log.debug("Error parsing date72:[" + str + "]:", ex);
+            }
+            try {
+                LocalDateTime dateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss.n"));
+                return dateTime;
+            } catch (Exception ex) {
+                log.debug("Error parsing date73:[" + str + "]:", ex);
+            }
+
         }
-        log.error("Unknown date format:["+str+"]");
+
+        if (str.contains("-") && str.contains(":") && str.contains(" ")) {
+            try {
+                ZonedDateTime dateTime = ZonedDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ssZ"));
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
+            } catch (Exception ex) {
+                log.debug("Error parsing date81:[" + str + "]:", ex);
+            }
+            try {
+                ZonedDateTime dateTime = ZonedDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ssX"));
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
+            } catch (Exception ex) {
+                log.debug("Error parsing date82:[" + str + "]:", ex);
+            }
+            try {
+                LocalDateTime dateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss"));
+                return dateTime;
+            } catch (Exception ex) {
+                log.debug("Error parsing date83:[" + str + "]:", ex);
+            }
+
+        }
+        try {
+            DateTime customDateTimeFromString = new DateTime(str);
+            if (customDateTimeFromString != null) {
+                // java.time.Instant
+                Instant instant = Instant.ofEpochMilli(customDateTimeFromString.getMillis());
+                // create the OffsetDateTime
+                LocalDateTime fromJoda = LocalDateTime.ofInstant(instant, ZoneId.of(customDateTimeFromString.getZone().getID()));
+                return fromJoda;
+            }
+        } catch (Exception ex) {
+            log.debug("Error parsing date7 joda:[" + str + "]:", ex);
+        }
+
+        if (str.contains(" ") && str.contains("-") && str.contains(":")) {
+            try {
+//                String substring = str.substring(0,19);
+                ZonedDateTime dateTime = ZonedDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss z"));
+                LocalDateTime dateTimeUTC = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneOffset.UTC);
+                return dateTimeUTC;
+            } catch (Exception ex) {
+                log.debug("Error parsing date9:[" + str + "]:", ex);
+            }
+        }
+
+        if (str.contains("+") && str.contains("-") && str.contains("Z")) {
+            try {
+                String substring = str.substring(0, 19);
+                LocalDateTime dateTime = LocalDateTime.parse(substring, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+                return dateTime;
+            } catch (Exception ex) {
+                log.debug("Error parsing date8:[" + str + "]:", ex);
+            }
+        }
+
+
+        if (isDdMonWordYear(str)) {
+            try {
+                DateTimeFormatter dTF = new DateTimeFormatterBuilder().parseCaseInsensitive()
+                        .appendPattern("dd-MMM-yyyy")
+                        .toFormatter(Locale.ENGLISH);
+                LocalDate dateTime = LocalDate.parse(str, dTF);
+                return dateTime.atStartOfDay();
+            } catch (Exception ex) {
+                log.debug("Error parsing date101:[" + str + "]:", ex);
+            }
+        }
+
+
+        if (str.contains(" ") && str.contains("#")) {
+            try {
+                LocalDate dateTime = LocalDate.parse(str.substring(0,8), DateTimeFormatter.ofPattern("yyyyMMdd"));
+                return dateTime.atStartOfDay();
+            } catch (Exception ex) {
+                log.debug("Error parsing date102:[" + str + "]:", ex);
+            }
+        }
+
+        if (str.contains(" ") && str.contains("/")  && str.contains(":")) {
+            try {
+                LocalDateTime dateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+                return dateTime;
+            } catch (Exception ex) {
+                log.debug("Error parsing date102:[" + str + "]:", ex);
+            }
+        }
+
+
+        if (str.contains(" ") && str.contains("-") && str.contains(":")) {
+            try {
+                String substring = str.substring(0, 19);
+                LocalDateTime dateTime = LocalDateTime.parse(substring, DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss"));
+                log.warn("Unknown date format:[" + str + "]. Parsing as \"yyyy-MM-dd' 'HH:mm:ss\"");
+                return dateTime;
+            } catch (Exception ex) {
+                log.debug("Error parsing date9:[" + str + "]:", ex);
+            }
+        }
+        if (str.contains("T") && str.contains("-") && str.contains(":") && str.contains("Z")) {
+            try {
+                String substring = str.substring(0, 19);
+                LocalDateTime dateTime = LocalDateTime.parse(substring, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+                log.warn("Unknown date format:[" + str + "]. Parsing as \"yyyy-MM-dd'T'HH:mm:ss\"");
+                return dateTime;
+            } catch (Exception ex) {
+                log.debug("Error parsing date9:[" + str + "]:", ex);
+            }
+        }
+        if (str.contains("T") && str.contains("-") && str.contains(":") && str.contains("UTC")) {
+            try {
+                String substring = str.substring(0, str.indexOf("UTC"));
+                LocalDateTime dateTime = LocalDateTime.parse(substring, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+                log.warn("Unknown date format:[" + str + "]. Parsing as \"yyyy-MM-dd'T'HH:mm:ss\"");
+                return dateTime;
+            } catch (Exception ex) {
+                log.debug("Error parsing date10:[" + str + "]:", ex);
+            }
+        }
+
+        log.error("Unknown date format:[" + str + "]");
         return null;
     }
     public static long getMilliseconds(LocalDateTime localDateTime) {
         if (localDateTime == null) return 0;
         return localDateTime.atZone(ZoneId.of("UTC")).toInstant().toEpochMilli();
+    }
+    public static boolean isDdMonWordYear(String input) {
+        String regex = "^\\d{2}-(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)-\\d{4}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input.toLowerCase());
+
+        return matcher.matches();
     }
 
 }
--- src/test/java/org/sitecenter/common/util/LocalDateUtilsTest.java
+++ src/test/java/org/sitecenter/common/util/LocalDateUtilsTest.java
@@ -6,23 +6,55 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LocalDateUtilsTest {
+class LocalLocalDateUtilsTest {
     @Test
     void getDate() {
 
-        assertEquals(LocalDateTime.of(2023, 4,30,12,22,36), LocalDateUtils.getDate("2023-04-30T12:22:36Z"));
-        assertEquals(LocalDateTime.of(2023, 1,13,18,17,48), LocalDateUtils.getDate("2023.01.13T18:17:48+0000"));
-        assertEquals(LocalDateTime.of(2023,1,13,18,17,48), LocalDateUtils.getDate("2023-01-13T18:17:48+0000"));
-        assertEquals(LocalDateTime.of(2003,6,29,18,24,40), LocalDateUtils.getDate("2003-06-29T18:24:40Z"));
-        assertEquals(LocalDateTime.of(2003,6,29,18,24,40), LocalDateUtils.getDate("2003-06-29T18:24:40"));
-        assertEquals(LocalDateTime.of(2003,6,29,0,0), LocalDateUtils.getDate("2003-06-29"));
-        assertEquals(LocalDateTime.of(2024,3,29,0,0), LocalDateUtils.getDate("2024.03.29"));
+        assertEquals(LocalDateTime.of(2023,04,30,12,22,36), LocalDateUtils.getDate("2023-04-30T12:22:36Z"));
+        assertEquals(LocalDateTime.of(2023,01,13,18,17,48), LocalDateUtils.getDate("2023.01.13T18:17:48+0000"));
+        assertEquals(LocalDateTime.of(2023,01,13,18,17,48), LocalDateUtils.getDate("2023-01-13T18:17:48+0000"));
+        assertEquals(LocalDateTime.of(2003,06,29,18,24,40), LocalDateUtils.getDate("2003-06-29T18:24:40Z"));
+        assertEquals(LocalDateTime.of(2003,06,29,18,24,40), LocalDateUtils.getDate("2003-06-29T18:24:40"));
+
+        assertEquals(LocalDateTime.of(2003,06,29,0,0), LocalDateUtils.getDate("2003-06-29"));
+        assertEquals(LocalDateTime.of(2024,03,29,0,0), LocalDateUtils.getDate("2024.03.29"));
         assertEquals(LocalDateTime.of(2022,8,24,2,17,5), LocalDateUtils.getDate("2022-08-24 02:17:05"));
-        assertEquals(LocalDateTime.of(2023,6,9,0,13,45), LocalDateUtils.getDate("2023-06-09T00:13:45+03:00"));
+        assertEquals(LocalDateTime.of(2023,6,8,21,13,45), LocalDateUtils.getDate("2023-06-09T00:13:45+03:00"));
         assert(LocalDateUtils.getDate("2023-06-fdlkjnsdfljk09T00:13:45+03:00") == null);
         assertEquals(LocalDateTime.of(2023,5,31,10,19,3), LocalDateUtils.getDate("2023-05-31T10:19:03Z "));
-        assertEquals(LocalDateTime.of(2023,3,3,20,43,29), LocalDateUtils.getDate("Fri Mar 03 17:43:29 UTC 2023"));
+        assertEquals(LocalDateTime.of(2023,9,22,10,46,31), LocalDateUtils.getDate("2023-09-22T10:46:31Z"));
+        assertEquals(LocalDateTime.of(1,1,1,0,0,0), LocalDateUtils.getDate("0001-01-01T00:00:00.00Z"));
 
+        assertEquals(LocalDateTime.of(2023,05,15,07,05,50), LocalDateUtils.getDate("2023-05-15t07:05:50z"));
+        assertEquals(LocalDateTime.of(2024,12,11,00,00,00), LocalDateUtils.getDate("2024-12-11T00:00:00+01:00Z"));
+
+        assertEquals(LocalDateTime.of(2024,06,24,00,00,00), LocalDateUtils.getDate("2024-06-24 00:00:00.000"));
+
+        //(KST) is 9 hours ahead of Coordinated Universal Time (UTC)
+        assertEquals(LocalDateTime.of(2023,06,27,01,39,58), LocalDateUtils.getDate("2023-06-27 10:39:58 KST"));
+
+        assertEquals(LocalDateTime.of(2021,9,17,8,40,10), LocalDateUtils.getDate("2021-09-17T08:40:10+0000Z"));
+
+
+        assertEquals(LocalDateTime.of(2023,9,11,5,5,42,722739), LocalDateUtils.getDate("2023-09-11 15:05:42.722739+10"));
+
+        assertEquals(LocalDateTime.of(1998,12,28,5,0,0), LocalDateUtils.getDate("1998-12-28T05:00:00Z.0Z"));
+
+        assertEquals(LocalDateTime.of(2023,5,26,4,10,32, 593), LocalDateUtils.getDate("2023-05-26 04:10:32.593"));
+
+        assertEquals(LocalDateTime.of(2024,11,18,9,22,59), LocalDateUtils.getDate("2024-11-18 20:22:59+11"));
+
+        assertEquals(LocalDateTime.of(2020,9,24,19,21,27), LocalDateUtils.getDate("2020-09-24T19:21:27UTC"));
+
+        assertEquals(LocalDateTime.of(2024,11,1,0,0,0), LocalDateUtils.getDate("01-Nov-2024"));
+
+        assertEquals(LocalDateTime.of(2023,10,31,0,0,0), LocalDateUtils.getDate("31-OCT-2023"));
+
+        assertEquals(LocalDateTime.of(2019,2,18,0,0,0), LocalDateUtils.getDate("20190218 #19329631"));
+
+        assertEquals(LocalDateTime.of(2024,6,28,0,59,59), LocalDateUtils.getDate("28/06/2024 00:59:59"));
+
+//        assertEquals(LocalDateTime.of(2015,9,29,4,57,40), LocalDateUtils.getDate("2015-29-09T04:57:40Z"));
 
 
     }
