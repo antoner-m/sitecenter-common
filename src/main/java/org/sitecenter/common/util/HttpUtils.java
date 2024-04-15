a8f562a57a686d6d45288f7d0cacf086682d2645
Anton <me@antoner.me>
2024-04-15 15:02:28 +0400

    httputils introduced.

---
+++ src/main/java/org/sitecenter/common/util/HttpUtils.java
@@ -0,0 +1,32 @@
+package org.sitecenter.common.util;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class HttpUtils {
+
+    public static byte[] download(String url) throws IOException {
+        URL urlObj = new URL(url);
+        try (BufferedInputStream in = new BufferedInputStream(urlObj.openStream());
+             ByteArrayOutputStream baStream = new ByteArrayOutputStream()) {
+            byte dataBuffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                baStream.write(dataBuffer, 0, bytesRead);
+            }
+            return baStream.toByteArray();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    public static String extractFileName(String url) throws URISyntaxException {
+        return Paths.get(new URI(url).getPath()).getFileName().toString();
+    }
+}
