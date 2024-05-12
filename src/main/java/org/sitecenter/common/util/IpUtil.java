45ef0003032ed564e19866081fdde403483d76c0
Anton <me@antoner.me>
2024-05-12 16:05:31 +0400

    parse ipv4 to long function.

--- src/main/java/org/sitecenter/common/util/IpUtil.java
+++ src/main/java/org/sitecenter/common/util/IpUtil.java
@@ -1,16 +1,11 @@
 package org.sitecenter.common.util;
 
+import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class IpUtil {
 
-    public static Long parseIPToLong(String ipAddress) {
-        if (ipAddress.contains(":"))
-            return parseIPv6ToLong(ipAddress);
-        else
-            return parseIPv4ToLong(ipAddress);
-    }
     public static Long parseIPv4ToLong(String ipAddress) {
         InetAddress inetAddress = null;
         try {
@@ -28,20 +23,12 @@ public class IpUtil {
         return result;
     }
 
-    public static Long parseIPv6ToLong(String ipAddress) {
-        InetAddress inetAddress = null;
-        try {
-            inetAddress = InetAddress.getByName(ipAddress);
-        } catch (UnknownHostException e) {
-            return null;
-        }
-        byte[] ipv6Bytes = inetAddress.getAddress();
-
-        long result = 0;
-        for (byte b : ipv6Bytes) {
-            result = (result << 8) | (b & 0xFF);
-        }
-
-        return result;
+    public static String longToIpv4(long iplong) {
+        StringBuilder result = new StringBuilder(16);
+        result.append(iplong>>24 & 0xFF).append(".");
+        result.append(iplong>>16 & 0xFF).append(".");
+        result.append(iplong>>8 & 0xFF).append(".");
+        result.append(iplong & 0xFF);
+        return result.toString();
     }
 }
