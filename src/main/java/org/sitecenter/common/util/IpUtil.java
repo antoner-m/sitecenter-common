a265f626a46e5017729e044cc130b29e6947e9a3
Anton <me@antoner.me>
2024-06-03 15:59:48 +0400

    parse ipv4 to long function.

--- src/main/java/org/sitecenter/common/util/IpUtil.java
+++ src/main/java/org/sitecenter/common/util/IpUtil.java
@@ -3,6 +3,8 @@ package org.sitecenter.common.util;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IpUtil {
 
@@ -23,6 +25,18 @@ public class IpUtil {
         return result;
     }
 
+    public static Long ipV4ToLong(String strIP) {
+        long[] ip = new long[4];
+        String[] ipSec = strIP.split("\\.");
+        if (ipSec.length != 4)
+            return null;
+        for (int k = 0; k < 4; k++) {
+            ip[k] = Long.valueOf(ipSec[k]);
+        }
+
+        return (ip[0] << 24) + (ip[1] << 16) + (ip[2] << 8) + ip[3];
+    }
+
     public static String longToIpv4(long iplong) {
         StringBuilder result = new StringBuilder(16);
         result.append(iplong>>24 & 0xFF).append(".");
@@ -31,4 +45,44 @@ public class IpUtil {
         result.append(iplong & 0xFF);
         return result.toString();
     }
+
+    public static List<String> range2cidrlist(String startIp, String endIp ) {
+        long start = ipV4ToLong(startIp);
+        long end = ipV4ToLong(endIp);
+
+        ArrayList<String> pairs = new ArrayList<String>();
+        while ( end >= start ) {
+            byte maxsize = 32;
+            while ( maxsize > 0) {
+                long mask = CIDR2MASK[ maxsize -1 ];
+                long maskedBase = start & mask;
+
+                if ( maskedBase != start ) {
+                    break;
+                }
+
+                maxsize--;
+            }
+            double x = Math.log( end - start + 1) / Math.log( 2 );
+            byte maxdiff = (byte)( 32 - Math.floor( x ) );
+            if ( maxsize < maxdiff) {
+                maxsize = maxdiff;
+            }
+            String ip = longToIpv4(start);
+            pairs.add( ip + "/" + maxsize);
+            start += Math.pow( 2, (32 - maxsize) );
+        }
+        return pairs;
+    }
+
+    public static final int[] CIDR2MASK = new int[] { 0x00000000, 0x80000000,
+            0xC0000000, 0xE0000000, 0xF0000000, 0xF8000000, 0xFC000000,
+            0xFE000000, 0xFF000000, 0xFF800000, 0xFFC00000, 0xFFE00000,
+            0xFFF00000, 0xFFF80000, 0xFFFC0000, 0xFFFE0000, 0xFFFF0000,
+            0xFFFF8000, 0xFFFFC000, 0xFFFFE000, 0xFFFFF000, 0xFFFFF800,
+            0xFFFFFC00, 0xFFFFFE00, 0xFFFFFF00, 0xFFFFFF80, 0xFFFFFFC0,
+            0xFFFFFFE0, 0xFFFFFFF0, 0xFFFFFFF8, 0xFFFFFFFC, 0xFFFFFFFE,
+            0xFFFFFFFF };
+
+
 }
--- src/test/java/org/sitecenter/common/util/IpUtilTest.java
+++ src/test/java/org/sitecenter/common/util/IpUtilTest.java
@@ -2,6 +2,9 @@ package org.sitecenter.common.util;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IpUtilTest {
@@ -21,5 +24,39 @@ class IpUtilTest {
         assertEquals(67438087, start2);
         long end2 = IpUtil.parseIPv4ToLong("60.70.80.90");
         assertEquals(1011241050, end2);
+    }
+
+    @Test
+    void cidrFromRange() {
+        List<String> cidrSrb = IpUtil.range2cidrlist("178.148.0.0", "178.148.255.255");
+        System.out.println(String.join(";", cidrSrb));
+        assertEquals(1, cidrSrb.size());
+        assertEquals("178.148.0.0/16", cidrSrb.get(0));
+
+        List<String> cidrSrb2 = IpUtil.range2cidrlist("178.148.0.6", "178.148.255.255");
+        System.out.println(String.join(";", cidrSrb2));
+        assertEquals(14, cidrSrb2.size());
+        assertEquals("178.148.0.6/31", cidrSrb2.get(0));
+
+        List<String> cidrTotal = IpUtil.range2cidrlist("0.0.0.0", "255.255.255.255");
+        System.out.println(String.join(";", cidrTotal));
+        assertEquals(1, cidrTotal.size());
+        assertEquals("0.0.0.0/0", cidrTotal.get(0));
+
+        List<String> cidr7 = IpUtil.range2cidrlist("0.0.0.0", "1.255.255.255");
+        System.out.println(String.join(";", cidr7));
+        assertEquals(1, cidr7.size());
+        assertEquals("0.0.0.0/7", cidr7.get(0));
+
+        List<String> cidr192 = IpUtil.range2cidrlist("192.168.0.0", "192.168.255.255");
+        System.out.println(String.join(";", cidr192));
+        assertEquals(1, cidr192.size());
+        assertEquals("192.168.0.0/16", cidr192.get(0));
+
+
+        List<String> cidr1922 = IpUtil.range2cidrlist("192.168.0.0", "192.168.0.255");
+        System.out.println(String.join(";", cidr1922));
+        assertEquals(1, cidr1922.size());
+        assertEquals("192.168.0.0/24", cidr1922.get(0));
     }
 }
