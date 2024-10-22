b3c6d272ee71ddc8beb9ee0394fe888709f3af50
Anton <me@antoner.me>
2024-10-22 10:17:37 +0400

    Add test for regexp pattern matching in page content
    Introduced a new test method `regexpOnPage` to verify if the regex pattern can correctly find and extract the Google ad client ID from the given HTML content. The test asserts that the regex is found and the extracted value matches the expected result.

--- src/test/java/org/sitecenter/common/util/RegexpResponseTest.java
+++ src/test/java/org/sitecenter/common/util/RegexpResponseTest.java
@@ -44,6 +44,29 @@ class RegexpResponseTest {
         RegexpResponse response = new RegexpResponse(regexp, content);
         assertFalse(response.isRegexpFound());
         assertEquals(0, response.getMatchGroupValues().size());
+    }
 
+    @Test
+    void regexpOnPage() {
+        String str = "/google_ad_client\\s*=\\s*[\"'](\\w+-?\\d+)[\"']/";
+        String regexp = str;
+        if (str.startsWith("/") && str.endsWith("/")) {
+            regexp = str.substring(1, str.length() - 1);
+        }
+        String content = "\n" +
+                "\n" +
+                "<script type=\"text/javascript\"><!--\n" +
+                "google_ad_client = \"pub-9652480419831112\";\n" +
+                "/* 728x90, 21.04.09 */\n" +
+                "google_ad_slot = \"980192111\";\n" +
+                "google_ad_width = 728;\n" +
+                "google_ad_height = 90;\n" +
+                "//-->\n" +
+                "</script>";
+
+        RegexpResponse response = new RegexpResponse(regexp, content);
+        assertTrue(response.isRegexpFound());
+        assertEquals(1, response.getMatchGroupValues().size());
+        assertEquals("pub-9652480419831112", response.getMatchGroupValues().get(0));
     }
 }
